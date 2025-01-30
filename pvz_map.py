1a1139104ceb19895c9f1e177b5129a1aba823f5
SoulHunteer <seregalutovinov@yandex.ru>
2025-01-30 14:07:23 +0300

    edit init driver

--- pvz_map.py
+++ pvz_map.py
@@ -312,10 +312,11 @@ def init_driver():
     logger.info("Инициализация Chrome драйвера")
     try:
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
         driver = webdriver.Chrome(options=chrome_options)
-        logger.info("Драйвер успешно инициализирован")
         return driver
     except Exception as e:
         logger.critical(f"Ошибка инициализации драйвера: {str(e)}")
